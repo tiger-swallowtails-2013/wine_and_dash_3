852708089c0266867cb7fe987557c17bd7b87bc6
Apprentice <apprentice@dbc17.local>
2013-10-04 00:05:22 -0700

    umm...not sure what i changed but i need to update the files

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,13 +1,3 @@
-
-<style type="text/css">
-html { height: 100% }
-body { height: 100%; margin: 0; padding: 0 }
-#map-canvas { height: 70%; width: 60%}
-</style>
-<script type="text/javascript"
-src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAw-fQLzljIV_Ynbkt0sJoVs3HIqJODsQQ&sensor=false">
-</script>
-<script type="text/javascript">
 function initialize() {
 
  var locations = [
@@ -37,8 +27,3 @@ var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions)
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-</script>
-
-<div id="map-canvas"></div>
-
