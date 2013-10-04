8269f25b3085ab5c65a5bdce1d2652703822a613
Apprentice <apprentice@dbc17.local>
2013-10-03 23:12:59 -0700

    clean up code for readability

--- user.rb
+++ user.rb
@@ -5,10 +5,13 @@ class User < ActiveRecord::Base
 	validates :username, uniqueness: true
 	validates :password, presence: true
 
-
 	# scope a
 	# attr_reader :username
 	def self.authenticate(username,password)
-		return false unless user = User.where(username: username, password: password).first else return user
+		unless user = User.where(username: username, password: password).first 
+			return false 
+		else 
+			return user
+		end
 	end
 end
